23aac5a97f1577b0a282d3305f2ed2601ed4618b
akulkarni3472 <kuldog@gmail.com>
2024-12-01 16:52:44 -0500

    Initial files

--- docusearch.py
+++ docusearch.py
@@ -4,6 +4,7 @@ import pandas
 from matplotlib import pyplot as plt
 import torch
 import torchvision
+from torch.nn.functional import cosine_similarity
 from transformers import AutoModel, AutoTokenizer
 import utils
 
@@ -16,5 +17,27 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
 # List of text from research ppaers
-documents = utils.extract_text_from_pdf(papers_path)
+documents = utils.extract_text_to_list_pydf2(papers_path)
 
+query = "Hormonal disorder"
+query_tokens = tokenizer(query, return_tensors='pt')
+
+with torch.no_grad():
+    query_embedding = model(**query_tokens).last_hidden_state.mean(dim=1)
+
+document_embeddings = []
+for doc in documents:
+    doc_tokens = tokenizer(doc, return_tensors='pt')
+    with torch.no_grad():
+        doc_embedding = model(**doc_tokens).last_hidden_state.mean(dim=1)
+        document_embeddings.append(doc_embedding)
+
+document_embeddings = torch.cat(document_embeddings)
+
+similarities = cosine_similarity(query_embedding, document_embeddings)
+
+num_results = 2
+top_k_indices = torch.topk(similarities, k=num_results).indices
+
+for index in top_k_indices:
+    print(f"Relevant Document: {documents[index]}")
--- utils.py
+++ utils.py
@@ -1,17 +1,27 @@
 import os
 import PyPDF2
+from pdfminer.high_level import extract_text
 
-def extract_text_from_pdf(parent_path):
+def extract_text_to_list_pydf2(parent_path):
     pdf_list = []
     for filename in os.listdir(parent_path):
         if filename.endswith('.pdf'):
             pdf_path = os.path.join(parent_path, filename)
 
             with open(pdf_path) as pdf_file:
-                reader = PyPDF2.PdfReader(pdf_path)
+                reader = PyPDF2.PdfReader(pdf_file)
                 text = ''
 
                 for page in reader.pages:
-                    text += page.extract_text + "\n"
+                    text += page.extract_text() + "\n"
+            pdf_list.append(text.strip())
+    return pdf_list
+
+def extract_text_to_list_pdfminer(parent_path):
+    pdf_list = []
+    for filename in os.listdir(parent_path):
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(parent_path, filename)
+            text = extract_text(pdf_path)
             pdf_list.append(text.strip())
     return pdf_list
